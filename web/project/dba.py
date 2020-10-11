75fe200a7222f468b7ea6ea73863873735e7c98b
İlke Elvan <ilkeelvan@gmail.com>
2020-10-11 03:10:37 +0300

    Resolve circle imports

--- web/project/dba.py
+++ web/project/dba.py
@@ -1,20 +1,8 @@
 from project.models import User, Pace
+from project import db
+from project.schemas import UserSchema
 
-def create_db(db):
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-
-def seed_db(db):
-    with open("data/users.csv", "r") as f:
-        for line in f.readlines():
-            id, username, age, gender = line.strip().split(",")
-            db.session.add(User(id=id, username=username, age=age,
-            gender=gender))
-    db.session.commit()
-    with open("data/pace.csv", "r") as f:
-        for line in f.readlines():
-            user_id, total_time, distance = line.strip().split(",")
-            db.session.add(Pace(user_id=user_id, total_time=total_time,
-            distance=distance))
-    db.session.commit()
+def get_values(sort="average_pace"):
+    user_schema = UserSchema()
+    print(user_schema.dump(User.query.first()))
+    return None
