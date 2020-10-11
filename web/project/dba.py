aa49a700d6aa1b9aa9ad228cc9db4356dc3de130
İlke Elvan <ilkeelvan@gmail.com>
2020-10-11 12:51:31 +0300

    Finish backend functionality

--- web/project/dba.py
+++ web/project/dba.py
@@ -2,10 +2,8 @@ from project.models import User, Pace
 from project import db
 from project.schemas import PaceSchema
 
-def get_values():
-
+def get_values(sort="average_pace"):
     schema = PaceSchema()
     paces = Pace.query.all()
-    data = [schema.dump(pace) for pace in paces ]
-    
-    return data
+    result = [schema.dump(pace) for pace in paces]
+    return result
--- web/project/views/api.py
+++ web/project/views/api.py
@@ -6,8 +6,8 @@ api_bp = Blueprint('api', __name__)
 @api_bp.route('/get_values', methods=["GET"])
 def get_values():
     results = dba.get_values()
-    
-    return jsonify(results)
+
+    return jsonify(result=results)
 
 @api_bp.route("/", methods=["GET"])
 def api_check():
